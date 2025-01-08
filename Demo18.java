4b90ca5944aa6809cd075677fd007f1e038c1a40
Sravanthireddy6 <gangapuramsravanthi36@gmail.com>
2025-01-08 11:27:57 +0530

    Converting Minutes to Years & Days

---
+++ Demo18.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Demo18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of minutes: ");
+        long minutes = scanner.nextLong();
+        long minutesInHour = 60;
+        long minutesInDay = 1440; // 60 minutes * 24 hours
+        long minutesInYear = 525600; // 60 minutes * 24 hours * 365 days
+        long years = minutes / minutesInYear;
+        long remainingMinutes = minutes % minutesInYear;
+        long days = remainingMinutes / minutesInDay;
+        System.out.println(minutes + " minutes is approximately " + years + " years and " + days + " days.");
+        scanner.close();
+    }
+}
+
