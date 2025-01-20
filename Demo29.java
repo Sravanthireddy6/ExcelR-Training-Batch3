cbb0a57f7eb9805fd4e327fbe7b697b95f79419d
Sravanthireddy6 <gangapuramsravanthi36@gmail.com>
2025-01-20 12:15:41 +0530

    Array List to store Friends Names

---
+++ Demo29.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class Demo29{
+    public static void main(String[] args) {
+        ArrayList<String> friends = new ArrayList<>();
+        friends.add("Sravanthi");
+        friends.add("Bindhu");
+        friends.add("Bhargavi");
+        friends.add("Hari");
+        friends.add("Bunny");
+        friends.add("Pandu");
+        friends.add("Gracy");
+        friends.add("Chintu");
+        friends.add("Anishvar");
+        friends.add("Bhavya");
+        System.out.println("Friends List: " + friends);
+    }
+}
