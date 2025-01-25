e4e4d08e710c1ab2ffe91c3e040c031e48e6834f
Sravanthireddy6 <gangapuramsravanthi36@gmail.com>
2025-01-25 13:01:20 +0530

    HW: Greater among 4 Numbers

--- Demo037.java
+++ Demo037.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Demo37 {
+public class Demo037 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter 1st number: ");
