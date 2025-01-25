240f5b8d29fe86680e650c4dde72580127beee4f
Sravanthireddy6 <gangapuramsravanthi36@gmail.com>
2025-01-25 13:00:05 +0530

    HW: Even or Odd

--- Demo034.java
+++ Demo034.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Demo34{
+public class Demo034{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
