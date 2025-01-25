fb1588fae03a7f633183d352b31c1b4e76a9e577
Sravanthireddy6 <gangapuramsravanthi36@gmail.com>
2025-01-25 13:01:41 +0530

    HW: Lower or Uppercase

--- Demo038.java
+++ Demo038.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Demo38 {
+public class Demo038 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a character: ");
