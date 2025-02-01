8fdd46b6c2fcf36a06d92632be1add727a8b8ad7
Sravanthireddy6 <gangapuramsravanthi36@gmail.com>
2025-02-01 13:14:07 +0530

    Greatest0f2

---
+++ PDemo002.java
@@ -0,0 +1,12 @@
+public class PDemo002 {
+    public static void main(String[] args){
+        int num1,num2;
+        num1=23;
+        num2=12;
+        if(num1 > num2){
+        System.out.println(num1 + " is the greatest");
+    }else {
+            System.out.println(num2 + " is the greatest");
+        }
+    }
+}
